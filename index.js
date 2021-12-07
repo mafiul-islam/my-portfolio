820d0a41b41366ee719be70c07e5e5730b4cd9e3
mafiul islam <mafiulislam.mi@gmail.com>
2021-12-07 20:34:02 +0600

    db added succefully

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express();
+const DB = 'mongodb+srv://mafiul:mafiul123@cluster0.xxcey.mongodb.net/myprofile?retryWrites=true&w=majority'
 
-app.get('/', (req, resp)=> {
-resp.send("application is running added to git...")
+mongoose.connect(DB).then(() => {
+    console.log(`connection successful`);
+}).catch((error) => console.log(`Error connection`));
+
+app.get('/', (req, resp) => {
+    resp.send("application is running added to git...")
 });
 
-app.listen(5000, ()=> {
+app.listen(5000, () => {
     console.log("server started at port 5000")
 })
